fdcb3fb40be41b16d4112f5a45ee3d6d2617e17e
Jessica Dittrich <104876367+jessicamdittrich@users.noreply.github.com>
2022-06-15 19:11:22 -0700

    Further work with tutor

---
+++ db/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const uuidv4 = require("uuid");
+const util = require("util");
+
+
+const readAsync = util.promisify(fs.readFile);
+
+class Notes {
+  read() {
+    return readAsync("db/db.json", "utf8");
+  }
+
+  readNotes() {
+    return this.read().then((notes) => {
+      let allNotes;
+      try {
+        allNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        allNotes = [];
+      }
+      return allNotes;
+    });
+  }
+
+//   writeNotes(note) {
+//     const { title, text } = note;
+
+//     const newNote = {
+//       title,
+//       text,
+//       tip_id: uuidv4(),
+//     };
+//   }
+}
+
+module.exports = new Notes();
--- server.js
+++ server.js
@@ -1,68 +1,42 @@
 // EXPRESS RELATED VARIABLES
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const uuid = require('uuid');
-
-// LINKING JSON FILE WHERE DATA IS STORED AND PULLED FROM
-const noteData = require('./develop/db/db.json');
+const express = require("express");
+const path = require("path");
+const db = require("./db");
 
 // INITIALIZE EXPRESS
 const app = express();
 
 // ENVIRONMENT PORT & LOCAL PORT VARIABLE
-const PORT = process.env.port || 3001;
-
-// ADDED IN STATIC MIDDLEWARE FOR SERVING ASSETS IN PUBLIC FOLDER
-app.use(express.static('./develop/public'));
+const PORT = process.env.PORT || 3001;
 
 // SETTING UP EXPRESS APPLICATION TO HANDLE DATA PARSING
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// ADDED IN STATIC MIDDLEWARE FOR SERVING ASSETS IN PUBLIC FOLDER
+app.use(express.static("public"));
 
-// TRANSFERS HTML FILE AT GIVEN PATH AND SETS THE HTTP HEADER FIELD
-/*app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});*/
-
-// GET ROUTE FOR HTML PAGE
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+// GET ROUTE FOR NOTES PAGE X2
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // GET ROUTE FOR NOTES PAGE
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-// INITIALIZING NEW VARIABLE FOR EXPRESS ROUTER
-const notes = require('express').Router();
-
-// 
-notes.get('notes.html', (req, res) => {
-    readFromFile('./develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+app.get("/api/notes", (req, res) => {
+  db.readNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
-//
-notes.post('notes.html', (req, res) => {
+// //
+// app.post('/api/notes', (req, res) => {
 
-    const { title, text } = req.body;
-
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            tip_id: uuid(),
-        };
-
-        readAndAppend(newNote, noteData);
-        res.json('Note added successfully');
-    } else {
-        res.error('Error adding in note...');
-    }
-});
+// });
 
 // CONFIRMING IN TERMINAL THAT CONNECTION IS WORKING
-app.listen(PORT, () =>
-    console.log(`listening at http://localhost:${PORT}`)
-)
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
